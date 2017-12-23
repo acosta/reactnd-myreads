9f977afe772e224d1af521fddfc75bfc7e12182d
Afonso R. Costa Jr <arabelo@gmail.com>
2017-12-23 10:42:35 -0400

    Set the shelf value in selector according to book's shelf
    Fix #10

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Book = ({ book }) => {
+const Book = ({ book, onShelfChange }) => {
+  const changeShelf = (event) => {
+    const shelf = event.target.value
+    onShelfChange(shelf, book)
+  }
+
   return (
     <div className="book">
       <div className="book-top">
@@ -14,10 +19,8 @@ const Book = ({ book }) => {
           }}
         />
         <div className="book-shelf-changer">
-          <select>
-            <option value="none" disabled>
-              Move to...
-            </option>
+          <select value={book.shelf || 'none'} onChange={changeShelf}>
+            <option value="none" disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
             <option value="read">Read</option>
